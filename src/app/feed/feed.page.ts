584a64774a1d295c8b392ab43c7c23b9310256a2
Afonso09 <afonsos921@gmail.com>
2020-05-14 15:51:36 -0300

    Aplicando funções typescript

--- src/app/feed/feed.page.ts
+++ src/app/feed/feed.page.ts
@@ -6,9 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed.page.scss'],
 })
 export class FeedPage implements OnInit {
-
+  public nome_usuario: string = "Afonso";
   constructor() { }
 
+  public somaDoisNumeros(num1:number, num2:number): void{
+    alert(num1 + num2);
+  }
+  ionViewDidLoad(){
+    this.somaDoisNumeros(10, 99);
+  }
   ngOnInit() {
   }
 
